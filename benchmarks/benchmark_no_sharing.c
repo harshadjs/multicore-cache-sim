fd179a61f13d8ef93c68222cd1a1526cfa5b0e2e
Harshad <harshad@cmu.edu>
2014-11-19 13:47:03 -0500

    Fixed bugs in benchmarks

--- benchmarks/benchmark_no_sharing.c
+++ benchmarks/benchmark_no_sharing.c
@@ -11,52 +11,39 @@
 
 /* Page size */
 #define PAGE_SIZ KB(4)
+#define N_PAGES_PER_THREAD 16
+#define N_THREADS 16
 
-/* Number of accesses performed by each thread */
-#define N_ACCESSES MB(16)
+struct my_page {
+	uint8_t data[PAGE_SIZ];
+} __attribute__((aligned(PAGE_SIZ)));
 
-uint8_t *data;
+struct my_page pages[N_PAGES_PER_THREAD * N_THREADS];
 
 void *thread_function(void *arg)
 {
-	volatile int sum = 0;
-	register int index;
-	int i, my_id = (int)(long)arg;
+	int i, j, my_id = (int)(long)arg;
 
-	for(i = 0; i < N_ACCESSES; i++) {
-		index = (my_id * 2) + (i % PAGE_SIZ);
-		sum += data[index];
-	}
+	for(i = 0; i < N_PAGES_PER_THREAD; i++)
+		for(j = 0; j < PAGE_SIZ; j++)
+			pages[(my_id * N_PAGES_PER_THREAD) + i].data[j]++;
+
 	return NULL;
 }
 
 int main(int argc, char *argv[])
 {
-	int i, n_threads;
-	pthread_t *threads;
+	int i;
+	pthread_t threads[N_THREADS];
 
-	if(argc < 2) {
-		printf("Usage: %s <number_of_threads>\n", argv[0]);
-		return 0;
-	}
-	n_threads = strtol(argv[1], NULL, 10);
-	threads = malloc(sizeof(pthread_t) * n_threads);
-
-	/*
-	 * Allocate 2 pages for each thread, so that, even if
-	 * the allocation is not aligned to 4KB page boundary,
-	 * we are good!
-	 */
-	data = malloc(2 * PAGE_SIZ * n_threads);
-
-	for(i = 0; i < n_threads; i++) {
+	for(i = 0; i < N_THREADS; i++) {
 		pthread_create(&threads[i],
 					   NULL,
 					   thread_function,
 					   (void *)(long)i);
 	}
 
-	for(i = 0; i < n_threads; i++) {
+	for(i = 0; i < N_THREADS; i++) {
 		pthread_join(threads[i], NULL);
 	}
 	return 0;
